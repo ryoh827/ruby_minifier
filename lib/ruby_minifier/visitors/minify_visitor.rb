ac3ef83d07d38f2da4a54bc71d46f82efd032b1f
ryoh827 <ryoh827.dev@gmail.com>
2025-03-24 04:20:10 +0900

    wip

--- lib/ruby_minifier/visitors/minify_visitor.rb
+++ lib/ruby_minifier/visitors/minify_visitor.rb
@@ -1,189 +1,204 @@
+require 'prism'
+
 module RubyMinifier
   module Visitors
     class MinifyVisitor < Prism::Visitor
-      OPERATOR_PRECEDENCE = {
-        "**" => 1,
-        "*" => 2, "/" => 2, "%" => 2,
-        "+" => 3, "-" => 3,
-        "<<" => 4, ">>" => 4,
-        "&" => 5,
-        "^" => 6,
-        "|" => 7,
-        "<=" => 8, ">=" => 8, "<" => 8, ">" => 8,
-        "==" => 9, "!=" => 9, "===" => 9,
-        "&&" => 10,
-        "||" => 11
-      }.freeze
-
       def initialize
-        super()
+        @result = []
       end
 
       def visit(node)
-        return "" unless node
-        node.accept(self)
+        super
+        @result.join
       end
 
       def visit_program_node(node)
-        visit_statements_node(node.statements)
+        visit_all(node.statements)
       end
 
       def visit_statements_node(node)
-        return "" unless node&.body
-        node.body.map { |n| visit(n) }.join(";")
+        node.body.each_with_index do |stmt, i|
+          visit(stmt)
+          @result << ";" unless i == node.body.length - 1 || stmt.is_a?(Prism::IfNode)
+        end
       end
 
       def visit_def_node(node)
-        name = node.name.to_s
-        params = visit(node.parameters)
-        body = visit(node.body)
-        "def #{name}#{params};#{body};end"
+        @result << "def "
+        @result << node.name
+        if node.parameters
+          @result << "("
+          visit(node.parameters)
+          @result << ")"
+        end
+        @result << ";"
+        visit(node.body)
+        @result << ";end"
       end
 
       def visit_class_node(node)
-        name = visit(node.constant_path)
-        body = visit(node.body)
-        "class #{name};#{body};end"
+        @result << "class "
+        visit(node.constant_path)
+        @result << ";"
+        visit(node.body)
+        @result << ";end"
       end
 
       def visit_constant_path_node(node)
-        if node.parent
-          "#{visit(node.parent)}::#{visit(node.child)}"
-        else
-          visit(node.child)
-        end
+        visit(node.child)
       end
 
       def visit_constant_read_node(node)
-        node.name.to_s
-      end
-
-      def visit_call_node(node)
-        receiver = visit(node.receiver)
-        name = node.name.to_s
-        args = visit(node.arguments)
-        block = visit(node.block)
-
-        if binary_operator?(name)
-          handle_binary_operator(receiver, name, args)
-        else
-          handle_method_call(receiver, name, args, block)
-        end
+        @result << node.name
       end
 
       def visit_string_node(node)
-        "\"#{node.content}\""
-      end
-
-      def visit_string_interpolation_node(node)
-        parts = node.parts.map do |part|
-          if part.is_a?(Prism::StringNode)
-            part.content
-          else
-            "\#{#{visit(part)}}"
-          end
-        end.join
-        "\"#{parts}\""
-      end
-
-      def visit_arguments_node(node)
-        return "" unless node&.arguments
-        node.arguments.map { |arg| visit(arg) }.join(",")
-      end
-
-      def visit_parameters_node(node)
-        return "" unless node
-        params = []
-        params.concat(node.requireds.map { |param| param.name }) if node.requireds
-        params.concat(node.optionals.map { |param| "#{param.name}=#{visit(param.value)}" }) if node.optionals
-        params << "*#{node.rest.name}" if node.rest
-        params.empty? ? "" : "(#{params.join(",")})"
-      end
-
-      def visit_local_variable_read_node(node)
-        node.name.to_s
-      end
-
-      def visit_local_variable_write_node(node)
-        "#{node.name}=#{visit(node.value)}"
-      end
-
-      def visit_instance_variable_read_node(node)
-        node.name.to_s
-      end
-
-      def visit_instance_variable_write_node(node)
-        "#{node.name}=#{visit(node.value)}"
+        @result << node.content.inspect
       end
 
       def visit_integer_node(node)
-        node.value.to_s
+        @result << node.value.to_s
       end
 
-      def visit_if_node(node)
-        predicate = visit(node.predicate)
-        statements = visit(node.statements)
-        else_clause = node.consequent ? ";else;#{visit(node.consequent)}" : ""
-        "if #{predicate};#{statements}#{else_clause};end"
+      def visit_float_node(node)
+        @result << node.value.to_s
       end
 
-      def visit_else_node(node)
-        visit(node.statements)
+      def visit_true_node(_)
+        @result << "true"
+      end
+
+      def visit_false_node(_)
+        @result << "false"
+      end
+
+      def visit_nil_node(_)
+        @result << "nil"
+      end
+
+      def visit_call_node(node)
+        if node.receiver
+          visit(node.receiver)
+          @result << "."
+        end
+        @result << node.name
+        if node.arguments
+          @result << "("
+          visit(node.arguments)
+          @result << ")"
+        end
+      end
+
+      def visit_arguments_node(node)
+        node.arguments.each_with_index do |arg, i|
+          visit(arg)
+          @result << "," unless i == node.arguments.length - 1
+        end
+      end
+
+      def visit_local_variable_read_node(node)
+        @result << node.name
+      end
+
+      def visit_local_variable_write_node(node)
+        @result << node.name
+        @result << "="
+        visit(node.value)
+      end
+
+      def visit_instance_variable_read_node(node)
+        @result << node.name
+      end
+
+      def visit_instance_variable_write_node(node)
+        @result << node.name
+        @result << "="
+        visit(node.value)
       end
 
       def visit_block_node(node)
-        params = visit(node.parameters)
-        body = visit(node.body)
-        " do#{params};#{body};end"
+        @result << " do"
+        if node.parameters
+          @result << "|"
+          visit(node.parameters)
+          @result << "|"
+        end
+        @result << ";"
+        visit(node.body)
+        @result << ";end"
       end
 
       def visit_block_parameters_node(node)
-        return "" unless node&.parameters
-        params = []
-        params.concat(node.parameters.requireds.map { |param| visit(param) }) if node.parameters.requireds
-        params.concat(node.parameters.optionals.map { |param| visit(param) }) if node.parameters.optionals
-        params << visit(node.parameters.rest) if node.parameters.rest
-        params.empty? ? "" : "|#{params.join(",")}|"
+        node.parameters.each_with_index do |param, i|
+          visit(param)
+          @result << "," unless i == node.parameters.length - 1
+        end
       end
 
-      private
-
-      def binary_operator?(name)
-        OPERATOR_PRECEDENCE.key?(name)
+      def visit_required_parameter_node(node)
+        @result << node.name
       end
 
-      def handle_binary_operator(left, operator, right)
-        current_precedence = OPERATOR_PRECEDENCE[operator]
-        left_precedence = get_precedence(left)
-        right_precedence = get_precedence(right)
-
-        left = parenthesize?(left_precedence, current_precedence) ? "(#{left})" : left
-        right = parenthesize?(right_precedence, current_precedence) ? "(#{right})" : right
-
-        "#{left}#{operator}#{right}"
+      def visit_binary_node(node)
+        visit(node.left)
+        @result << node.operator
+        visit(node.right)
       end
 
-      def handle_method_call(receiver, name, args, block)
-        receiver_part = receiver.empty? ? "" : "#{receiver}."
-        args_part = args.empty? ? "" : "(#{args})"
-        "#{receiver_part}#{name}#{args_part}#{block}"
+      def visit_if_node(node)
+        @result << "if "
+        visit(node.predicate)
+        @result << ";"
+        visit(node.statements)
+        if node.consequent
+          @result << ";else;"
+          visit(node.consequent)
+        end
+        @result << ";end"
       end
 
-      def get_precedence(node)
-        return OPERATOR_PRECEDENCE[node.operator] if node.is_a?(Prism::BinaryNode)
-        nil
+      def visit_array_node(node)
+        @result << "["
+        node.elements.each_with_index do |elem, i|
+          visit(elem)
+          @result << "," unless i == node.elements.length - 1
+        end
+        @result << "]"
       end
 
-      def parenthesize?(child_precedence, parent_precedence)
-        child_precedence && child_precedence > parent_precedence
+      def visit_hash_node(node)
+        @result << "{"
+        node.elements.each_with_index do |elem, i|
+          visit(elem)
+          @result << "," unless i == node.elements.length - 1
+        end
+        @result << "}"
+      end
+
+      def visit_assoc_node(node)
+        visit(node.key)
+        @result << ":"
+        visit(node.value)
+      end
+
+      def visit_symbol_node(node)
+        @result << ":"
+        @result << node.value
+      end
+
+      def visit_string_interpolation_node(node)
+        @result << "\""
+        node.parts.each do |part|
+          if part.is_a?(Prism::StringNode)
+            @result << part.content
+          else
+            @result << "\#{"
+            visit(part)
+            @result << "}"
+          end
+        end
+        @result << "\""
       end
     end
   end
-end 
-
-
-
-
-
-
-
+end
